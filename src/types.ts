b614c0a768d4009b31a6bc8267575e6601e37765
DiegoContreras25 <133098011+DiegoContreras25@users.noreply.github.com>
2025-12-18 16:54:32 +0100

    Add files via upload

---
+++ src/types.ts
@@ -0,0 +1,32 @@
+
+export type OwnedPokemon = {
+  _id: string;          
+  pokemonId: string;    
+  nickname: string;
+  level: number;
+};
+
+
+export type PokemonUser = {
+  _id: string;
+  name: string;
+  pokemons: OwnedPokemon[];
+};
+
+
+export type PokemonType =
+    "NORMAL"
+    "FIRE"
+    "WATER"
+    "ELECTRIC"
+    "GRASS"
+    "ICE"
+    "FIGHTING"
+    "POISON"
+    "GROUND"
+    "FLYING"
+    "PSYCHIC"
+    "BUG"
+    "ROCK"
+    "GHOST"
+    "DRAGON";
